d9cfaeb1664d85e115eba176103d8f85f3c934cd
Andrey Chayko <andreychayko@minsk-dev-mac72.by-minsk.csi.local>
2019-11-29 19:21:13 +0300

    added some other changes

--- webframework/src/index.ts
+++ webframework/src/index.ts
@@ -1,1 +1,8 @@
 import { User } from './models/User';
+const user = new User({ name: 'new record', age: 33 });
+
+user.events.on('change', () => {
+  console.log('Change!');
+});
+
+user.events.trigger('change');
