9f692296505205de53a41a8ad585cf6b39b6e725
Andrey Chayko <chaiko.work@gmail.com>
2020-06-30 22:01:58 +0300

    decorators

--- TypeScript-Express/src/index.ts
+++ TypeScript-Express/src/index.ts
@@ -1,11 +1,33 @@
-import express, { Request, Response } from 'express';
-import { router } from './routes/loginRoutes';
-import bodyParser from 'body-parser';
-const app = express();
+// import express, { Request, Response } from 'express';
+// import { router } from './routes/loginRoutes';
+// import bodyParser from 'body-parser';
+// import cookkieSession from 'cookie-session';
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(router);
+// const app = express();
 
-app.listen(3000, () => {
-  console.log('Listening on port 3000');
-});
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(cookkieSession({ keys: [''] }));
+// app.use(router);
+
+// app.listen(3000, () => {
+//   console.log('Listening on port 3000');
+// });
+
+
+class Boat {
+  color: string = 'red';
+
+  get formattedColor(): string {
+    return `This boats color is ${this.color}`;
+  }
+
+  @testDecorator
+  pilot(): void {
+    console.log('swish');
+  }
+}
+
+function testDecorator(target: any, key: string): void {
+  console.log('Target:', target);
+  console.log('Key:', key);
+}
